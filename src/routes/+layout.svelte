4ade69d497ace0f70d61783d5b3ae5a7fd7546a9
Ethan Nguyen <manhnguyend8@gmail.com>
2024-01-09 11:34:06 +0700

    Fix footer links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -88,14 +88,14 @@
 	<slot class="order-1 md:order-2 overflow-auto" />
 </div>
 
-<footer class="footer flex items-center justify-between bg-base-300 text-base-content p-2 h-12">
+<footer class="footer flex items-center justify-between bg-base-300 text-base-content px-4 py-2 h-12">
 	<p class="text-base">Made by Ethan Nguyen</p>
-	<span class="flex items-center">
-		<a href="https://github.com/manhnguyen48" target="_blank" aria-label="GitHub">
+	<span class="flex items-center gap-2">
+		<a href="https://github.com/manhnguyen48" target="_blank" aria-label="GitHub" rel="noopener noreferrer">
 		{@html github}</a>
-		<a href="https://www.linkedin.com/in/ethannguyen48/" target="_blank" aria-label="LinkedIn">
+		<a href="https://www.linkedin.com/in/ethannguyen48/" target="_blank" aria-label="LinkedIn" rel="noopener noreferrer">
 		{@html linkedin}</a>
-		<a href="https://twitter.com/bananadata48" target="_blank" aria-label="Twitter">
+		<a href="https://twitter.com/bananadata48" target="_blank" aria-label="Twitter" rel="noopener noreferrer">
 		{@html twitter}</a>
 	</span>
 </footer>
