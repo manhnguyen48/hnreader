58457a0edf47563024bc26fa3b5d02c715776d2f
Ethan Nguyen <manhnguyend8@gmail.com>
2024-01-13 15:16:49 +0000

    Clean up server code

--- src/routes/[feed=feedNames]/+page.server.ts
+++ src/routes/[feed=feedNames]/+page.server.ts
@@ -12,19 +12,20 @@ const app = initializeApp(firebaseConfig);
 // Initialize Realtime Database and get a reference to the service
 const db = getDatabase(app);
 
+// Function to query a single post given an ID
+const getPost = async (id: number) => {
+	const snapshot = await get(child(ref(db), `v0/item/${id}`));
+	return snapshot.val();
+}
+
 export const load: PageServerLoad = async ({ params, setHeaders }) => {
 	const feed = params.feed + 'stories';
 	const snapshot = await get(child(ref(db), `v0/${feed}`));
 
 	if (snapshot.exists()) {
 		const postIds = snapshot.val();
-		const posts = await Promise.all(
-			postIds.map(async (id: number) => {
-				const snapshot = await get(child(ref(db), `v0/item/${id}`));
-				return snapshot.val();
-			})
-		);
-		// Cache data for 5 minutes so we don't query the server too much
+		const posts = await Promise.all(postIds.map(getPost)); //TODO: Maybe change to Promise.allSettled here
+		// Cache data so we don't query the server too much
 		setHeaders({
 			'cache-control': 'public, max-age=120, must-revalidate'
 		})
