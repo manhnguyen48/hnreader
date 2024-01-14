0f3d480e12efcaac3ea4cde1e55213d1f12aaae0
Ethan Nguyen <manhnguyend8@gmail.com>
2024-01-14 08:48:33 +0000

    Change theme persistence to cookie

--- src/params/feedNames.ts
+++ src/params/feedNames.ts
@@ -1,6 +1,6 @@
 import type {ParamMatcher} from '@sveltejs/kit'
 
 export const match: ParamMatcher = (param) => {
-    const feeds: string[] = ['top', 'best', 'new', 'ask', 'show', 'job'];
+    const feeds: string[] = ['top', 'best', 'new', 'ask', 'show'];
     return feeds.includes(param)
 }
