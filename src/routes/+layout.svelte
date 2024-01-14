8902f8dcad6ffcfe2b08c18eab0fd594cdd14895
Ethan Nguyen <manhnguyend8@gmail.com>
2024-01-14 13:05:42 +0000

    Minor change to navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -71,11 +71,11 @@
 				<li>
 					<a
 						href="/{feedName}"
-						class="rounded-2xl {$page.url.pathname === `/${feedName}` ? 'active' : ''}"
+						class="rounded-xl {$page.url.pathname === `/${feedName}`
+							? 'active font-medium'
+							: 'font-light'}"
 					>
-						<span class="text-sm md:text-base font-light w-8 flex justify-center capitalize"
-							>{feedName}</span
-						>
+						<span class="text-sm md:text-base w-8 flex justify-center capitalize">{feedName}</span>
 					</a>
 				</li>
 			{/each}
