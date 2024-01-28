1d3d34ab359ac201fb828863e06f1dd561bb8686
Ethan Nguyen <manhnguyend8@gmail.com>
2024-01-28 15:47:45 +0000

    Increase menu bar height for PWA on mobile

--- src/routes/[feed=feedNames]/+layout.svelte
+++ src/routes/[feed=feedNames]/+layout.svelte
@@ -5,7 +5,7 @@
 
 <!-- Reverse the layout so on smaller screens, menu bar is bottom -->
 <div class="flex h-full w-full flex-col-reverse md:flex-col">
-	<div class="sticky bottom-0 z-[1] flex-1 backdrop-blur-md md:top-0">
+	<div class="sticky bottom-0 z-[1] min-h-16 flex-1 backdrop-blur-md md:top-0 md:min-h-fit">
 		<MenuBar />
 	</div>
 	<div class="min-h-screen flex-1 overflow-auto">
