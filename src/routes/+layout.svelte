a1b194d671e3076468cacd17ccc6e1b7c169964f
Ethan Nguyen <manhnguyend8@gmail.com>
2024-01-28 15:38:44 +0000

    Add PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,25 @@
 	import { Search } from 'lucide-svelte';
 	import { page } from '$app/stores';
 	import { searchPageShortcut } from '$lib/shortcuts';
+	import { onMount } from 'svelte';
+
+	async function detectSWUpdate() {
+		const registration = await navigator.serviceWorker.ready;
+
+		registration.addEventListener('updatefound', () => {
+			const newSW = registration.installing;
+			newSW?.addEventListener('statechange', () => {
+				if (newSW.state === 'installed') {
+					if (confirm('New update available! Reload to update')) {
+						newSW.postMessage({ type: 'SKIP_WAITING' });
+						window.location.reload();
+					}
+				}
+			});
+		});
+	}
+
+	onMount(() => detectSWUpdate());
 </script>
 
 <svelte:window on:keydown={searchPageShortcut} />
