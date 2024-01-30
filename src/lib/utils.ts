86f6dabe2dc437bdd5c701b3baa4a2d92a6155e7
Ethan Nguyen <manhnguyend8@gmail.com>
2024-01-30 23:57:55 +0000

    clean up time stamp function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,24 +6,20 @@ export function sanitiseHTML(input: string): string {
 }
 
 export function timeDifference(previous: number): string {
-	const sPerMinute: number = 60;
-	const sPerHour: number = sPerMinute * 60;
-	const sPerDay: number = sPerHour * 24;
-	const sPerMonth: number = sPerDay * 30;
-	const sPerYear: number = sPerDay * 365;
-	const elapsed: number = Date.now() / 1000 - previous;
+	const units = [
+		{ name: 's', value: 1 },
+		{ name: 'm', value: 60 },
+		{ name: 'h', value: 60 * 60 },
+		{ name: 'd', value: 60 * 60 * 24 },
+		{ name: 'M', value: 60 * 60 * 24 * 30 },
+		{ name: 'Y', value: 60 * 60 * 24 * 365 }
+	];
+	const elapsed = Date.now() / 1000 - previous;
 
-	if (elapsed < sPerMinute) {
-		return Math.round(elapsed / 1000) + 's';
-	} else if (elapsed < sPerHour) {
-		return Math.round(elapsed / sPerMinute) + 'm';
-	} else if (elapsed < sPerDay) {
-		return Math.round(elapsed / sPerHour) + 'h';
-	} else if (elapsed < sPerMonth) {
-		return Math.round(elapsed / sPerDay) + 'd';
-	} else if (elapsed < sPerYear) {
-		return Math.round(elapsed / sPerMonth) + 'M';
-	} else {
-		return Math.round(elapsed / sPerYear) + 'Y';
+	for (let i = units.length - 1; i >= 0; i--) {
+		if (elapsed >= units[i].value) {
+			return Math.round(elapsed / units[i].value) + units[i].name;
+		}
 	}
+	return 'now';
 }
